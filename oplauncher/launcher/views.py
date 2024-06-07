fda3372cb9df45c494fe758b457449a7a85e6832
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-06-07 06:42:57 +0000

    Index completed

--- oplauncher/launcher/views.py
+++ oplauncher/launcher/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Quals
 
 # Create your views here.
+
+def index(request):
+    quals = Quals.objects.all()
+    return render(request, 'launcher/index.html', {
+        'quals': quals,
+        
+    })
