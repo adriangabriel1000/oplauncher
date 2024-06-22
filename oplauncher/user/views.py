c42d07feab163cec3f4c40587d9e39ce9b273415
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-06-22 10:40:09 +0000

    userloging done

--- oplauncher/user/views.py
+++ oplauncher/user/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.http import HttpResponse
 
 # Create your views here.
 def user_login(request):
-    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("User authenticated and logged in")
+            else:
+                return HttpResponse("Invalid Credentails")
+    else:
+        form = LoginForm()
+
     return render(request, 'user/login.html',{
         'form' : form
     })
