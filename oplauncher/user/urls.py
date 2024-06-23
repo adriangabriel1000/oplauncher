60ff12118bd9b1c67a31569931b4f2e8393ea56d
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-06-23 08:53:26 +0000

    m

--- oplauncher/user/urls.py
+++ oplauncher/user/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('logout/', TemplateView.as_view(template_name='user/logout.html'), name='logout'),
     #path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
