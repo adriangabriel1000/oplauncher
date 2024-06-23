2c766a648cb5d33f7f994e35f80df07c3c2d4dd0
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-06-23 10:42:38 +0000

    m

--- oplauncher/user/urls.py
+++ oplauncher/user/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', TemplateView.as_view(template_name='user/logout.html'), name='logout'),
     #path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
-    
+
 ]
