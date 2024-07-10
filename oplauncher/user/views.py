8adc461b66545c9491246a802355cba694c9af66
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-07-10 15:33:36 +0000

    m

--- oplauncher/user/views.py
+++ oplauncher/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistraionForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -32,6 +32,20 @@ def user_logout(request):
 
 def accSettings(request):
     return render(request, 'user/account_settings.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistraionForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(request, '')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, '', {
+        'user_form': user_form
+    })
 
 def index(request):
     return render(request, 'user/index.html')
