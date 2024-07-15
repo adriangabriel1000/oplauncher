1806dbaf20c9f9641cbfdcc80a9b9031ceafbe51
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-07-15 09:58:07 +0000

    prf

--- oplauncher/launcher/views.py
+++ oplauncher/launcher/views.py
@@ -7,9 +7,9 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     quals = Quals.objects.all()
-    
     return render(request, 'launcher/index.html', {
         'quals': quals,
+
     })
 
 def accSettigns(request):
