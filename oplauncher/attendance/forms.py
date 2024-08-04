782e5e7bd765a48f1aa76e41ded2fbcf32bbe1a1
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-08-04 06:29:33 +0000

    unspecified

--- oplauncher/attendance/forms.py
+++ oplauncher/attendance/forms.py
@@ -1,8 +1,17 @@
 from .models import Attendance
 from django import forms
 
+
+class DateInput(forms.DateInput):
+    input_type = 'datetime-local'
+
+
 class AttendanceForm(forms.ModelForm):
     class Meta:
         model = Attendance
         fields = ('user', 'startTime', 'endTime', 'position', 'location')
+        widgets={
+            'startTime': DateInput(),
+            'endTime': DateInput(),      
+        }
 
