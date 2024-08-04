852b492c328bfb8b8d3b66d33b23dd9f4e614e31
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-08-04 18:45:06 +0000

    timeFilter

--- oplauncher/attendance/forms.py
+++ oplauncher/attendance/forms.py
@@ -4,21 +4,29 @@ from django import forms
 
 class AttendanceForm(forms.ModelForm):
     
-    pos = (
-        ("XM", "XM"),
-        ("XA", "XA"),
-        ("XN", "XN"),
-        ("XD", "XD"),
-        ("XZ", "XZ"),
-    )
+#    pos = (
+#        ("XM", "XM"),
+#        ("XA", "XA"),
+#        ("XN", "XN"),
+#        ("XD", "XD"),
+#        ("XZ", "XZ"),
+#    )
     
-    position = forms.ChoiceField(choices=pos, widget=forms.RadioSelect())
+    #position = forms.ChoiceField(choices=pos, widget=forms.RadioSelect())
     
     class Meta:
+        pos = (
+            ("XM", "XM"),
+            ("XA", "XA"),
+            ("XN", "XN"),
+            ("XD", "XD"),
+            ("XZ", "XZ"),
+        )
         model = Attendance
         fields = ('startTime', 'endTime', 'position', 'location')
         widgets={
             'startTime': forms.DateInput(attrs={'type': 'datetime-local'}),
             'endTime': forms.DateInput(attrs={'type': 'datetime-local'}),
+            'position': forms.Select(choices=pos, attrs={'class': 'form-select'}),
         }
 
--- oplauncher/attendance/views.py
+++ oplauncher/attendance/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .forms import AttendanceForm
 from .models import Attendance
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+#from django.utils import timezone
 
 
 # Create your views here.
@@ -22,8 +24,27 @@ def attendance_create(request):
 
 @login_required
 def attendance(request):
-    attendance = Attendance.objects.all()
+    #stDt = timezone.now().time()
+    day = datetime.now().day
+    month = datetime.now().month
+    year = datetime.now().year
+    hour = datetime.now().hour + 2
+    minute = datetime.now().minute
+    
+    if datetime(year, month, day, hour, minute) > datetime(year, month, day , 7, 0) and datetime(year, month, day, hour, minute) < datetime(year, month, day, 15, 0):
+        stTme = datetime(year, month, day , 7, 0)
+        enTme = datetime(year, month, day, 15, 0)
+    
+    if datetime(year, month, day, hour, minute) > datetime(year, month, day , 15, 0) and datetime(year, month, day, hour, minute) < datetime(year, month, day, 23, 0):
+        stTme = datetime(year, month, day , 15, 0)
+        enTme = datetime(year, month, day, 23, 0)
+    
+    if datetime(year, month, day, hour, minute) > datetime(year, month, day , 23, 0) and datetime(year, month, day+1, hour, minute) < datetime(year, month, day+1, 7, 0):
+        stTme = datetime(year, month, day , 23, 0)
+        enTme = datetime(year, month, day+1, 7, 0)
+    
+    attendance = Attendance.objects.filter(startTime__gte=stTme, endTime__lte=enTme)
+    #print(year, month, day, hour, minute)
     return render(request, 'attendance/attendance.html', {
         'attendance': attendance,
-
     })
