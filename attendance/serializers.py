526f7ba9fd12ff7bebe9357831530e796540a31b
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-08-11 18:05:41 +0000

    restCompleted

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -2,6 +2,21 @@ from rest_framework import serializers
 from .models import Attendance
 
 class AttendanceSerializer(serializers.ModelSerializer):
+    
+    def getUsername(self, obj):
+        return obj.user.username
+    
+    def getFirstName(self, obj):
+        return obj.user.first_name
+    
+    def getLastName(self, obj):
+        return obj.user.last_name
+
+    username = serializers.SerializerMethodField("getUsername")
+    firstName = serializers.SerializerMethodField("getFirstName")
+    lastName = serializers.SerializerMethodField("getLastName")
+    
+    
     class Meta:
         model = Attendance
-        fields = ('user', 'startTime', 'endTime', 'position', 'location')
+        fields = ('user', 'username', 'firstName', 'lastName', 'startTime', 'endTime', 'position', 'location')
